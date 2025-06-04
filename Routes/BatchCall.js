98ac4704e54eedec1349910549f8d25ac4ccc7e6
Biplab Mondal <biplab.mondal@webviotechnologies.com>
2025-06-04 11:14:07 +0530

    added req.body to get data from the body

--- Routes/BatchCall.js
+++ Routes/BatchCall.js
@@ -1,19 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Retell = require('retell-sdk');
-require('dotenv').config();
+const Retell = require("retell-sdk");
+require("dotenv").config();
 
 const client = new Retell({ apiKey: process.env.RETELL_API_KEY });
 
 // Route to create phone number
 router.post("/store", async (req, res) => {
   try {
-    const {
-      from_number,
-      tasks,
-      name,
-      trigger_timestamp,
-    } = req;
+    const { from_number, tasks, name, trigger_timestamp } = req.body;
 
     const phoneNumberResponse = await client.batchCall.createBatchCall({
       from_number,
@@ -24,13 +19,13 @@ router.post("/store", async (req, res) => {
 
     res.status(200).json({
       status: true,
-      data: phoneNumberResponse
+      data: phoneNumberResponse,
     });
   } catch (error) {
-    console.error('Retell API error:', error?.response?.data || error.message);
+    console.error("Retell API error:", error?.response?.data || error.message);
     res.status(500).json({
       status: false,
-      error: error || 'Something went wrong'
+      error: error || "Something went wrong",
     });
   }
 });
